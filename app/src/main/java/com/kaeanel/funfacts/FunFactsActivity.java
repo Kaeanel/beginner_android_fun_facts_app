d405970e7e041746c2ea0b2ff90dca74c01f861e
Denis <denisfilipov.12@gmail.com>
2020-03-03 23:36:57 +0100

    added a new class to randomize our main activity background color

--- app/src/main/java/com/kaeanel/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/kaeanel/funfacts/FunFactsActivity.java
@@ -1,19 +1,25 @@
 package com.kaeanel.funfacts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
     private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
     // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
+    private ConstraintLayout mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +29,17 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assigns the Views from the layout files to the corresponding variables
         factTextView = findViewById(R.id.factTextView);
         showFactButton = findViewById(R.id.showFactButton);
+        mainView = findViewById(R.id.mainView);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = factBook.getFact();
+                int color = colorWheel.getColor();
                 // Update the screen with our new fact
                 factTextView.setText(fact);
+                mainView.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
 
